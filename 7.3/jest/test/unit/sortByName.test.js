114cddccdf3c38a0a4618442fb9082d2801c3a6c
Vitaliy <Groteckon@mail.ru>
2024-02-12 15:06:50 +0300

    Задача 7.3.1

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,36 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
+    const input = [
         "Гарри Поттер",
         "Властелин Колец",
         "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+      ];
+    
+    const result = sorting.sortByName(input);
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+    expect(result).toEqual(expected);
+  }); 
+  
+  it("No sorting", () => {
+    const input = [
+      "Старик и море",
+      "Капитан Сорви голова",
+      "Капитан Сорви голова",
+      "Сказка странствий",
+    ];
+  
+    const result = sorting.sortByName(input);
+    const expected = [
+    "Капитан Сорви голова",
+    "Капитан Сорви голова",
+    "Сказка странствий",
+    "Старик и море",
+  ];
+  expect(result).toEqual(expected);
   });
 });
